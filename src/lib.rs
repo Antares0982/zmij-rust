b77a08254445e7693e7231022a64feb6ddb23c51
David Tolnay <dtolnay@gmail.com>
2025-12-21 07:42:50 -0800

    Add crosslink icons to top of rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! [![github]](https://github.com/dtolnay/zmij)&ensp;[![crates-io]](https://crates.io/crates/zmij)&ensp;[![docs-rs]](https://docs.rs/zmij)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
+//!
+//! <br>
+//!
 //! A double-to-string conversion algorithm based on [Schubfach] and [yy].
 //!
 //! This Rust implementation is a line-by-line port of Victor Zverovich's
